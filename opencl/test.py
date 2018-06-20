7212bc1d2be383d1000b2007a4424320140242a5
Victor Chen <yvictorck@gmail.com>
2018-06-20 03:08:03 -0700

    a

--- opencl/test.py
+++ opencl/test.py
@@ -8,10 +8,10 @@ import os
 import heartbeat
 shm_id = int(os.environ['VIC_SHM_ID'])
 window_size_hr=5
-print('creating hb')
+print('		creating hb')
 ts=time.time()
 hb = heartbeat.Heartbeat(shm_id,window_size_hr,100,"vic.log",10,100)
-print('time took to create hb',time.time()-ts)
+print('		time took to create hb',time.time()-ts)
 #hb.heartbeat_beat()
 #print(hb.get_instant_heartrate())
 
@@ -25,17 +25,17 @@ num_ele=1001
 num1 = np.array(range(num_ele), dtype=np.int32)
 num2 = np.array(range(num_ele), dtype=np.int32)
 out = np.empty(num1.shape, dtype=np.int32)
-print("correct answer",num1[-1]**3+num2[-1]**3)
+print("		correct answer",num1[-1]**3+num2[-1]**3)
 ts=time.time()
 ans=[]
 hb.heartbeat_beat()
-print('hb init hr:',hb.get_instant_heartrate())
+print('		hb init hr:',hb.get_instant_heartrate())
 for i in range(len(num1)):
 	ans.append(num1[i]**3+num2[i]**3)
-print('cpu exec time:',time.time()-ts)
+print('		cpu exec time:',time.time()-ts)
 hb.heartbeat_beat()
-print('hb: cpu exec time:',1/hb.get_instant_heartrate())
-
+print('		hb: cpu exec time:',1/hb.get_instant_heartrate())
+print('		start opencl:')
 # create the buffers to hold the values of the input
 num1_buf = cl.Buffer(cntxt, cl.mem_flags.READ_ONLY | cl.mem_flags.COPY_HOST_PTR,hostbuf=num1)
 num2_buf = cl.Buffer(cntxt, cl.mem_flags.READ_ONLY | cl.mem_flags.COPY_HOST_PTR,hostbuf=num2)
@@ -61,11 +61,11 @@ launch.wait()
 cl.enqueue_read_buffer(queue, out_buf, out).wait()
 hb.heartbeat_beat()
 tts=time.time()
-print('hb: gpu exec time:',1/hb.get_instant_heartrate())
-print('gpu exec time',tts-ts)
+print('		hb: gpu exec time:',1/hb.get_instant_heartrate())
+print('		gpu exec time',tts-ts)
 # print the output
-print("Number1:", num1[-1])
-print("Number2:", num2[-1])
-print("Output :", out[-1])
+print("		Number1:", num1[-1])
+print("		Number2:", num2[-1])
+print("		Output :", out[-1])
 
 hb.heartbeat_finish()
