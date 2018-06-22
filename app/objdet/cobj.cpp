29269e116338fdeb4ec478663884eb50e71cdd40
Victor Chen <yvictorck@gmail.com>
2018-06-22 10:48:04 -0700

    a

--- app/objdet/cobj.cpp
+++ app/objdet/cobj.cpp
@@ -6,19 +6,35 @@ using namespace cv;
 int main(int argc, char** argv)
 {
     Mat img, gray;
-	int start_s=clock();
-
+	
+    int start_s=clock();
     img = imread("image.jpeg", IMREAD_COLOR);
-     
     cvtColor(img, gray, COLOR_BGR2GRAY);
     GaussianBlur(gray, gray,Size(7, 7), 1.5);
     Canny(gray, gray, 0, 50);
      
-    imshow("edges", gray);
-    waitKey();
+    // imshow("edges", gray);
+    // waitKey();
 
 	// the code you wish to time goes here
 	int stop_s=clock();
 	std::cout << "time: " << (stop_s-start_s)/double(CLOCKS_PER_SEC)*1000 << endl;
-    return 0;
+
+
+
+    UMat uimg, ugray;
+    int ustart_s=clock();
+
+    imread("image.jpeg", IMREAD_COLOR).copyTo(uimg);
+     
+    cvtColor(uimg, ugray, COLOR_BGR2GRAY);
+    GaussianBlur(ugray, ugray,Size(7, 7), 1.5);
+    Canny(ugray, ugray, 0, 50);
+     
+    // imshow("edges", gray);
+    // waitKey();
+	int ustop_s=clock();
+	std::cout << "time: " << (ustop_s-ustart_s)/double(CLOCKS_PER_SEC)*1000 << endl;
+
+
 }
