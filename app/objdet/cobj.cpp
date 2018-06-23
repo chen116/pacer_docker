7fd4dce0055be30c0884ea0d6b0f264c2c6c77f6
Victor Chen <yvictorck@gmail.com>
2018-06-22 20:50:21 -0700

    a

--- app/objdet/cobj.cpp
+++ app/objdet/cobj.cpp
@@ -20,34 +20,35 @@ int main(int argc, char** argv)
 {
     Mat img, gray;
 	
-    int start_s=clock();
     img = imread("cat.jpg", IMREAD_COLOR);
+    int start_s=clock();
+
     cvtColor(img, gray, COLOR_BGR2GRAY);
     GaussianBlur(gray, gray,Size(7, 7), 1.5);
     Canny(gray, gray, 0, 50);
-     
-    imshow("edges", gray);
-    waitKey();
-
 	// the code you wish to time goes here
 	int stop_s=clock();
 	std::cout << "time: " << (stop_s-start_s)/double(CLOCKS_PER_SEC)*1000 << std::endl;
 
+    imshow("edges", gray);
+    waitKey();
+
+
 
 
     UMat uimg, ugray;
-    int ustart_s=clock();
 
     imread("cat.jpg", IMREAD_COLOR).copyTo(uimg);
-     
+    int ustart_s=clock();
     cvtColor(uimg, ugray, COLOR_BGR2GRAY);
     GaussianBlur(ugray, ugray,Size(7, 7), 1.5);
     Canny(ugray, ugray, 0, 50);
-     
+ 	int ustop_s=clock();
+	std::cout << "time: " << (ustop_s-ustart_s)/double(CLOCKS_PER_SEC)*1000 << std::endl;
+
     imshow("edges", gray);
     waitKey();
-	int ustop_s=clock();
-	std::cout << "time: " << (ustop_s-ustart_s)/double(CLOCKS_PER_SEC)*1000 << std::endl;
+
 
 
 }
