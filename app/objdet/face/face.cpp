8d7a818f581da3fba78645e05ca74720c4e8cffe
Victor Chen <yvictorck@gmail.com>
2018-06-25 21:32:32 -0700

    a

--- app/objdet/face/face.cpp
+++ app/objdet/face/face.cpp
@@ -48,7 +48,7 @@ int main( int argc, const char** argv )
         }
         //-- 3. Apply the classifier to the frame
         auto start = high_resolution_clock::now();
-        udetectAndDisplay( frame );
+        detectAndDisplay( frame );
         auto stop = high_resolution_clock::now();
     auto duration = duration_cast<microseconds>(stop - start);
  
