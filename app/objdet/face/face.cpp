f99f43deec698a30d2b45a9e00835d572b5a2ff5
Victor Chen <yvictorck@gmail.com>
2018-06-25 21:16:09 -0700

    a

--- app/objdet/face/face.cpp
+++ app/objdet/face/face.cpp
@@ -72,24 +72,24 @@ void udetectAndDisplay( Mat xframe )
     cvtColor( frame, frame_gray, COLOR_BGR2GRAY );
     equalizeHist( frame_gray, frame_gray );
     //-- Detect faces
-    face_cascade.detectMultiScale( frame_gray, faces, 1.1, 2, 0|CASCADE_SCALE_IMAGE, Size(30, 30) );
-    for ( size_t i = 0; i < faces.size(); i++ )
-    {
-        Point center( faces[i].x + faces[i].width/2, faces[i].y + faces[i].height/2 );
-        ellipse( frame, center, Size( faces[i].width/2, faces[i].height/2 ), 0, 0, 360, Scalar( 255, 0, 255 ), 4, 8, 0 );
-        UMat faceROI = frame_gray( faces[i] );
-        std::vector<Rect> eyes;
-        //-- In each face, detect eyes
-        eyes_cascade.detectMultiScale( faceROI, eyes, 1.1, 2, 0 |CASCADE_SCALE_IMAGE, Size(30, 30) );
-        for ( size_t j = 0; j < eyes.size(); j++ )
-        {
-            Point eye_center( faces[i].x + eyes[j].x + eyes[j].width/2, faces[i].y + eyes[j].y + eyes[j].height/2 );
-            int radius = cvRound( (eyes[j].width + eyes[j].height)*0.25 );
-            circle( frame, eye_center, radius, Scalar( 255, 0, 0 ), 4, 8, 0 );
-        }
-    }
-    //-- Show what you got
-    imshow( window_name, frame );
+    // face_cascade.detectMultiScale( frame_gray, faces, 1.1, 2, 0|CASCADE_SCALE_IMAGE, Size(30, 30) );
+    // for ( size_t i = 0; i < faces.size(); i++ )
+    // {
+    //     Point center( faces[i].x + faces[i].width/2, faces[i].y + faces[i].height/2 );
+    //     ellipse( frame, center, Size( faces[i].width/2, faces[i].height/2 ), 0, 0, 360, Scalar( 255, 0, 255 ), 4, 8, 0 );
+    //     UMat faceROI = frame_gray( faces[i] );
+    //     std::vector<Rect> eyes;
+    //     //-- In each face, detect eyes
+    //     eyes_cascade.detectMultiScale( faceROI, eyes, 1.1, 2, 0 |CASCADE_SCALE_IMAGE, Size(30, 30) );
+    //     for ( size_t j = 0; j < eyes.size(); j++ )
+    //     {
+    //         Point eye_center( faces[i].x + eyes[j].x + eyes[j].width/2, faces[i].y + eyes[j].y + eyes[j].height/2 );
+    //         int radius = cvRound( (eyes[j].width + eyes[j].height)*0.25 );
+    //         circle( frame, eye_center, radius, Scalar( 255, 0, 0 ), 4, 8, 0 );
+    //     }
+    // }
+    // //-- Show what you got
+    // imshow( window_name, frame );
 }
 
 void detectAndDisplay( Mat frame )
@@ -99,22 +99,22 @@ void detectAndDisplay( Mat frame )
     cvtColor( frame, frame_gray, COLOR_BGR2GRAY );
     equalizeHist( frame_gray, frame_gray );
     //-- Detect faces
-    face_cascade.detectMultiScale( frame_gray, faces, 1.1, 2, 0|CASCADE_SCALE_IMAGE, Size(30, 30) );
-    for ( size_t i = 0; i < faces.size(); i++ )
-    {
-        Point center( faces[i].x + faces[i].width/2, faces[i].y + faces[i].height/2 );
-        ellipse( frame, center, Size( faces[i].width/2, faces[i].height/2 ), 0, 0, 360, Scalar( 255, 0, 255 ), 4, 8, 0 );
-        Mat faceROI = frame_gray( faces[i] );
-        std::vector<Rect> eyes;
-        //-- In each face, detect eyes
-        eyes_cascade.detectMultiScale( faceROI, eyes, 1.1, 2, 0 |CASCADE_SCALE_IMAGE, Size(30, 30) );
-        for ( size_t j = 0; j < eyes.size(); j++ )
-        {
-            Point eye_center( faces[i].x + eyes[j].x + eyes[j].width/2, faces[i].y + eyes[j].y + eyes[j].height/2 );
-            int radius = cvRound( (eyes[j].width + eyes[j].height)*0.25 );
-            circle( frame, eye_center, radius, Scalar( 255, 0, 0 ), 4, 8, 0 );
-        }
-    }
-    //-- Show what you got
-    imshow( window_name, frame );
+    // face_cascade.detectMultiScale( frame_gray, faces, 1.1, 2, 0|CASCADE_SCALE_IMAGE, Size(30, 30) );
+    // for ( size_t i = 0; i < faces.size(); i++ )
+    // {
+    //     Point center( faces[i].x + faces[i].width/2, faces[i].y + faces[i].height/2 );
+    //     ellipse( frame, center, Size( faces[i].width/2, faces[i].height/2 ), 0, 0, 360, Scalar( 255, 0, 255 ), 4, 8, 0 );
+    //     Mat faceROI = frame_gray( faces[i] );
+    //     std::vector<Rect> eyes;
+    //     //-- In each face, detect eyes
+    //     eyes_cascade.detectMultiScale( faceROI, eyes, 1.1, 2, 0 |CASCADE_SCALE_IMAGE, Size(30, 30) );
+    //     for ( size_t j = 0; j < eyes.size(); j++ )
+    //     {
+    //         Point eye_center( faces[i].x + eyes[j].x + eyes[j].width/2, faces[i].y + eyes[j].y + eyes[j].height/2 );
+    //         int radius = cvRound( (eyes[j].width + eyes[j].height)*0.25 );
+    //         circle( frame, eye_center, radius, Scalar( 255, 0, 0 ), 4, 8, 0 );
+    //     }
+    // }
+    // //-- Show what you got
+    // imshow( window_name, frame );
 }
