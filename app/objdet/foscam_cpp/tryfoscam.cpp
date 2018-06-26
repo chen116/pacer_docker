4cec238854dc98adaa168d8ec13af0995105e88a
Victor Chen <yvictorck@gmail.com>
2018-06-25 19:55:47 -0700

    a

--- app/objdet/foscam_cpp/tryfoscam.cpp
+++ app/objdet/foscam_cpp/tryfoscam.cpp
@@ -1,7 +1,8 @@
 
-#include "opencv2/opencv.hpp"
-#include <opencv2\highgui\highgui.hpp>
-#include <opencv2\imgproc\imgproc.hpp>
+#include <opencv2/opencv.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
+
 #include <iostream>
 #include <stdio.h>
 
