696f717c1b38f5a7197e7ad2aa7093397d45a58c
Victor Chen <yvictorck@gmail.com>
2018-06-25 20:16:55 -0700

    a

--- app/objdet/foscam_cpp/tryfoscam.cpp
+++ app/objdet/foscam_cpp/tryfoscam.cpp
@@ -13,8 +13,8 @@ int main()
 {
     Mat frame;
     namedWindow("video", 1);
-    // VideoCapture cap("http://admin:admin@@65.114.169.151:88/videoMain.cgi?resolution=640x360&req_fps=50&.mjpg");
-    VideoCapture cap("http://65.114.169.151:88/cgi-bin/CGIStream.cgi?cmd=GetMJStream&usr=admin&pwd=admin.mjpg");
+    VideoCapture cap("http://admin:admin@@65.114.169.151:88/videoMain.cgi?&.mjpg");
+    // VideoCapture cap("http://65.114.169.151:88/cgi-bin/CGIStream.cgi?cmd=GetMJStream&usr=admin&pwd=admin.mjpg");
 
     if(!cap.isOpened())
     {
