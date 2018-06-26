4ea407dc8d959837b67257503424df6d85913d9e
Victor Chen <yvictorck@gmail.com>
2018-06-25 20:52:17 -0700

    a

--- app/objdet/face/face.cpp
+++ app/objdet/face/face.cpp
@@ -7,6 +7,7 @@
 using namespace std;
 using namespace cv;
 void detectAndDisplay( Mat frame );
+void udetectAndDisplay( UMat frame );
 String face_cascade_name, eyes_cascade_name;
 CascadeClassifier face_cascade;
 CascadeClassifier eyes_cascade;
@@ -47,6 +48,39 @@ int main( int argc, const char** argv )
     }
     return 0;
 }
+
+    UMat uimg, ugray;
+
+    .copyTo(uimg);
+void udetectAndDisplay( Mat xframe )
+{
+
+    UMat frame,frame_gray;
+    xframe.copyTo(frame)
+    std::vector<Rect> faces;
+    cvtColor( frame, frame_gray, COLOR_BGR2GRAY );
+    equalizeHist( frame_gray, frame_gray );
+    //-- Detect faces
+    face_cascade.detectMultiScale( frame_gray, faces, 1.1, 2, 0|CASCADE_SCALE_IMAGE, Size(30, 30) );
+    for ( size_t i = 0; i < faces.size(); i++ )
+    {
+        Point center( faces[i].x + faces[i].width/2, faces[i].y + faces[i].height/2 );
+        ellipse( frame, center, Size( faces[i].width/2, faces[i].height/2 ), 0, 0, 360, Scalar( 255, 0, 255 ), 4, 8, 0 );
+        Mat faceROI = frame_gray( faces[i] );
+        std::vector<Rect> eyes;
+        //-- In each face, detect eyes
+        eyes_cascade.detectMultiScale( faceROI, eyes, 1.1, 2, 0 |CASCADE_SCALE_IMAGE, Size(30, 30) );
+        for ( size_t j = 0; j < eyes.size(); j++ )
+        {
+            Point eye_center( faces[i].x + eyes[j].x + eyes[j].width/2, faces[i].y + eyes[j].y + eyes[j].height/2 );
+            int radius = cvRound( (eyes[j].width + eyes[j].height)*0.25 );
+            circle( frame, eye_center, radius, Scalar( 255, 0, 0 ), 4, 8, 0 );
+        }
+    }
+    //-- Show what you got
+    imshow( window_name, frame );
+}
+
 void detectAndDisplay( Mat frame )
 {
     std::vector<Rect> faces;
