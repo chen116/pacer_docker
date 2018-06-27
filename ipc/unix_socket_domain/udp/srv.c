9f522575b7f450b4beaf835e0d2481627d383487
Victor Chen <yvictorck@gmail.com>
2018-06-26 22:10:04 -0700

    a

---
+++ ipc/unix_socket_domain/udp/srv.c
@@ -0,0 +1,62 @@
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/un.h>
+
+/*
+ * In the included file <sys/un.h> a sockaddr_un is defined as follows
+ * struct sockaddr_un {
+ *  short   sun_family;
+ *  char    sun_path[108];
+ * };
+ */
+
+
+#include <stdio.h>
+
+
+#define NAME "socket"
+
+
+#define SERVER_PATH "tpf_unix_sock.server"
+#define CLIENT_PATH "tpf_unix_sock.client"
+#define DATA "Hello from client"
+/*
+ * This program creates a UNIX domain datagram socket, binds a name to it,
+ * then reads from the socket.
+ */
+int main()
+{
+    int sock, length;
+    struct sockaddr_un name;
+    char buf[1024];
+
+
+    /* Create socket from which to read. */
+    sock = socket(AF_UNIX, SOCK_DGRAM, 0);
+    if (sock < 0) {
+        perror("opening datagram socket");
+        exit(1);
+    }
+
+
+    /* Create name. */
+    name.sun_family = AF_UNIX;
+    strcpy(name.sun_path, NAME);
+
+
+    /* Bind the UNIX domain address to the created socket */
+    if (bind(sock, (struct sockaddr *) &name, sizeof(struct sockaddr_un))) {
+        perror("binding name to datagram socket");
+        exit(1);
+    }
+    printf("socket -->%s\n", NAME);
+
+
+    /* Read from the socket */
+    if (read(sock, buf, 1024) < 0)
+        perror("receiving datagram packet");
+    printf("-->%s\n", buf);
+    close(sock);
+    unlink(NAME);
+}
+
