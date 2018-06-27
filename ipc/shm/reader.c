09ff805cc38e56aab9089fa2f75e9cd3327f3dbc
Victor Chen <yvictorck@gmail.com>
2018-06-27 03:59:07 -0700

    a

--- ipc/shm/reader.c
+++ ipc/shm/reader.c
@@ -14,26 +14,36 @@ struct shared_data {
 };
 int main(int argc, char *argv[]) {
 	int shared_seg_size = (1 * sizeof(struct shared_data));
-	printf("meow\n");
 	int shmfd  = shm_open(SHMOBJ_PATH, O_RDONLY, 0666);
-	printf("meow\n");
 
 	struct shared_data * shared_msg = (struct shared_data *) mmap(NULL, shared_seg_size, PROT_READ, MAP_SHARED, shmfd, 0);
-	printf("meow\n");
+	
 	
 	sem_t * sem_id = sem_open(SEM_PATH, 0);
-	printf("meow\n");
+	
 	
 	struct shared_data in_msg;
-	printf("meow\n");
+	
 	
 	sem_wait(sem_id);
+	printf("meow\n");
+
 	/* Update shared data */
 	memcpy(&in_msg, shared_msg, sizeof(struct shared_data));
+	printf("meow\n");
+
 	sem_post(sem_id);
+	printf("meow\n");
+
 	/* Process data… */
 	shm_unlink(SHMOBJ_PATH);
+	printf("meow\n");
+
 	sem_close(sem_id);
+	printf("meow\n");
+
 	sem_unlink(SEM_PATH);
+	printf("meow\n");
+	
 	return 0;
 }
