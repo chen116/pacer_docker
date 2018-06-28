6080730dc011b18c30b6db4ab12658e1c3d9f35c
Victor Chen <yvictorck@gmail.com>
2018-06-27 21:51:13 -0700

    a

--- ipc/shm/example/sysv/producer.c
+++ ipc/shm/example/sysv/producer.c
@@ -22,7 +22,7 @@ int main(int argc, const char *argv[])
     while (temp_buf[0]!='1') {
 
     fgets (temp_buf, 1, stdin);
-    item = temp_buf[0]-'0';//produce_item();
+    item = temp_buf-'0';//produce_item();
     printf("well my int is %d\n",item);
     semop(semid, &downEmpty, 1);
     semop(semid, &downMutex, 1);
