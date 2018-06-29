1bae0994b985d493adc33b7dac106487194771a1
Victor Chen <yvictorck@gmail.com>
2018-06-29 00:56:54 -0700

    a

--- ipc/shm/srv.cpp
+++ ipc/shm/srv.cpp
@@ -5,7 +5,7 @@
 
 struct shared_memory_buffer
 {
-   enum { NumItems = 1 };
+   enum { NumItems = 5 };
 
    shared_memory_buffer()
       : mutex(1), nempty(1), nstored(0)
@@ -28,22 +28,21 @@ using namespace boost::interprocess;
 
 int main ()
 {
-   //Remove shared memory on destruction
+   const int NumMsg = 10;
+   
    struct shm_remove
    {
-      ~shm_remove(){ shared_memory_object::remove("MySharedMemory");shared_memory_object::remove("docker_MySharedMemory"); }
+      shm_remove() { shared_memory_object::remove("MySharedMemory"); shared_memory_object::remove("vic_MySharedMemory");}
+      ~shm_remove(){ shared_memory_object::remove("MySharedMemory"); shared_memory_object::remove("vic_MySharedMemory");}
    } remover;
-
-
-
-
 
    //Create a shared memory object.
    shared_memory_object shm
-      (open_or_create                  //only create
+      (create_only                  //only create
       ,"MySharedMemory"              //name
       ,read_write  //read-write mode
       );
+
    //Set size
    shm.truncate(sizeof(shared_memory_buffer));
 
@@ -56,49 +55,51 @@ int main ()
    //Get the address of the mapped region
    void * addr       = region.get_address();
 
-   //Obtain the shared structure
-   // shared_memory_buffer * data = new (addr) shared_memory_buffer;
-   shared_memory_buffer * data = static_cast<shared_memory_buffer*>(addr);
-
-   const int NumMsg = 1;
-
-   int extracted_data [NumMsg];
-   //Extract the data
-   printf("goonna wait now\n");
-   data->nstored.wait();
-   data->mutex.wait();
-   extracted_data[0] = data->items[0];
-   printf("srv got: %d\n",extracted_data[0]);
-   data->mutex.post();
-   data->nempty.post();
+   //Construct the shared structure in memory
+   shared_memory_buffer * data = new (addr) shared_memory_buffer;
 
 
    //Create a shared memory object.
-   shared_memory_object docker_shm
-      (open_only                  //only create
-      ,"docker_MySharedMemory"              //name
+   shared_memory_object vic_shm
+      (create_only                  //only create
+      ,"vic_MySharedMemory"              //name
       ,read_write  //read-write mode
       );
+
+   //Set size
+   vic_shm.truncate(sizeof(shared_memory_buffer));
+
    //Map the whole shared memory in this process
-   mapped_region docker_region
-      (docker_shm                       //What to map
+   mapped_region vic_region
+      (vic_shm                       //What to map
       ,read_write //Map it as read-write
       );
+   void * vic_addr       = vic_region.get_address();
 
-   //Get the address of the mapped docker_region
-   void * docker_addr       = docker_region.get_address();
-
-   //Construct the shared structure in memory
-   shared_memory_buffer * docker_data = new (docker_addr) shared_memory_buffer;
-   docker_data->nempty.wait();
-   docker_data->mutex.wait();
-   docker_data->items[0] = extracted_data[0];
-   printf("srv wrote: %d\n",docker_data->items[0] );
-   docker_data->mutex.post();
-   docker_data->nstored.post();
+   //Obtain the shared structure
+   shared_memory_buffer * vic_data = static_cast<shared_memory_buffer*>(vic_addr);
 
 
 
+//Extract the data
+   for(int i = 0; i < NumMsg; ++i){
+      data->nstored.wait();
+      data->mutex.wait();
+      extracted_data[i] = data->items[i % shared_memory_buffer::NumItems];
+      printf("got: %d\n",extracted_data[i]);
+      data->mutex.post();
+      data->nempty.post();
+   }
+
+   //Insert data in the array
+   for(int i = 0; i < NumMsg; ++i){
+      vic_data->nempty.wait();
+      vic_data->mutex.wait();
+      vic_data->items[i % shared_memory_buffer::NumItems] = i+10;
+      printf("wrote: %d\n",i );
+      vic_data->mutex.post();
+      vic_data->nstored.post();
+   }
    return 0;
 }
 
