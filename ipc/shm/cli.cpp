7ec57e44030239e68c59d8f3ff6e0fc7a313b943
Victor Chen <yvictorck@gmail.com>
2018-06-28 23:54:29 -0700

    a

--- ipc/shm/cli.cpp
+++ ipc/shm/cli.cpp
@@ -1,5 +1,6 @@
 // w create 2 mem, w read one, r write anthoer
-//The second process opens the shared memory and copies the received integers to it's own buffer:
+//This is the process main process. Creates the shared memory, 
+//places there the integer array and starts integers one by one, blocking if the array is full:
 
 #include <boost/interprocess/sync/interprocess_semaphore.hpp>
 
@@ -18,7 +19,6 @@ struct shared_memory_buffer
    //Items to fill
    int items[NumItems];
 };
-
 #include <boost/interprocess/shared_memory_object.hpp>
 #include <boost/interprocess/mapped_region.hpp>
 #include <iostream>
@@ -28,40 +28,22 @@ using namespace boost::interprocess;
 
 int main ()
 {
-   //Remove shared memory on destruction
+   //Remove shared memory on construction and destruction
    struct shm_remove
    {
-      ~shm_remove(){ shared_memory_object::remove("MySharedMemory");shared_memory_object::remove("vic_MySharedMemory"); }
+      shm_remove() { shared_memory_object::remove("MySharedMemory"); shared_memory_object::remove("vic_MySharedMemory");}
+      ~shm_remove(){ shared_memory_object::remove("MySharedMemory"); shared_memory_object::remove("vic_MySharedMemory");}
    } remover;
 
    //Create a shared memory object.
-   shared_memory_object vic_shm
-      (open_only                  //only create
-      ,"vic_MySharedMemory"              //name
-      ,read_write  //read-write mode
-      );
-   //Map the whole shared memory in this process
-   mapped_region vic_region
-      (vic_shm                       //What to map
-      ,read_write //Map it as read-write
-      );
-
-   //Get the address of the mapped vic_region
-   void * vic_addr       = vic_region.get_address();
-
-   //Construct the shared structure in memory
-   shared_memory_buffer * vic_data = new (vic_addr) shared_memory_buffer;
-
-
-
-
-
-   //Create a shared memory object.
    shared_memory_object shm
-      (open_only                    //only create
+      (create_only                  //only create
       ,"MySharedMemory"              //name
       ,read_write  //read-write mode
       );
+
+   //Set size
+   shm.truncate(sizeof(shared_memory_buffer));
 
    //Map the whole shared memory in this process
    mapped_region region
@@ -72,40 +54,63 @@ int main ()
    //Get the address of the mapped region
    void * addr       = region.get_address();
 
-   //Obtain the shared structure
-   shared_memory_buffer * data = static_cast<shared_memory_buffer*>(addr);
+   //Construct the shared structure in memory
+   shared_memory_buffer * data = new (addr) shared_memory_buffer;
+
+
+   //Create a shared memory object.
+   shared_memory_object vic_shm
+      (create_only                  //only create
+      ,"vic_MySharedMemory"              //name
+      ,read_write  //read-write mode
+      );
+
+   //Set size
+   vic_shm.truncate(sizeof(shared_memory_buffer));
+
+   //Map the whole shared memory in this process
+   mapped_region vic_region
+      (vic_shm                       //What to map
+      ,read_write //Map it as read-write
+      );
+
 
    const int NumMsg = 10;
 
-   int extracted_data [NumMsg];
+   //Insert data in the array
+   for(int i = 0; i < NumMsg; ++i){
+      data->nempty.wait();
+      data->mutex.wait();
+      data->items[i % shared_memory_buffer::NumItems] = i;
+      printf("wrote: %d\n",i );
+      data->mutex.post();
+      data->nstored.post();
+   }
+
+
+   void * vic_addr       = vic_region.get_address();
+
+   //Obtain the shared structure
+   shared_memory_buffer * vic_data = static_cast<shared_memory_buffer*>(vic_addr);
+   int extracted_vic_data [NumMsg];
 
    //Extract the data
    for(int i = 0; i < NumMsg; ++i){
-      data->nstored.wait();
-      data->mutex.wait();
-      extracted_data[i] = data->items[i % shared_memory_buffer::NumItems];
-      printf("got: %d\n",extracted_data[i]);
-      data->mutex.post();
-      data->nempty.post();
-   }
-
-   //Insert data in the array
-   for(int i = 0; i < NumMsg; ++i){
-      vic_data->nempty.wait();
+      vic_data->nstored.wait();
       vic_data->mutex.wait();
-      vic_data->items[i % shared_memory_buffer::NumItems] = i+10;
-      printf("wrote: %d\n",i );
+      extracted_vic_data[i] = vic_data->items[i % shared_memory_buffer::NumItems];
+      printf("got: %d\n",extracted_vic_data[i]);
       vic_data->mutex.post();
-      vic_data->nstored.post();
+      vic_data->nempty.post();
    }
-
 
    return 0;
 }
 
-// w create sharedMem, open vic_shared. r read sharedMem, create vic_shared
 
-// //The second process opens the shared memory and copies the received integers to it's own buffer:
+// w create sharedMem, open vic_shared. r read sharedMem, create vic_shared
+// //This is the process main process. Creates the shared memory, 
+// //places there the integer array and starts integers one by one, blocking if the array is full:
 
 // #include <boost/interprocess/sync/interprocess_semaphore.hpp>
 
@@ -124,7 +129,6 @@ int main ()
 //    //Items to fill
 //    int items[NumItems];
 // };
-
 // #include <boost/interprocess/shared_memory_object.hpp>
 // #include <boost/interprocess/mapped_region.hpp>
 // #include <iostream>
@@ -134,21 +138,54 @@ int main ()
 
 // int main ()
 // {
-//    //Remove shared memory on destruction
+//    //Remove shared memory on construction and destruction
 //    struct shm_remove
 //    {
-//       ~shm_remove(){ shared_memory_object::remove("MySharedMemory");shared_memory_object::remove("vic_MySharedMemory"); }
+//       shm_remove() { shared_memory_object::remove("MySharedMemory"); shared_memory_object::remove("vic_MySharedMemory");}
+//       ~shm_remove(){ shared_memory_object::remove("MySharedMemory"); shared_memory_object::remove("vic_MySharedMemory");}
 //    } remover;
 
 //    //Create a shared memory object.
-//    shared_memory_object vic_shm
+//    shared_memory_object shm
 //       (create_only                  //only create
-//       ,"vic_MySharedMemory"              //name
+//       ,"MySharedMemory"              //name
 //       ,read_write  //read-write mode
 //       );
 
 //    //Set size
-//    vic_shm.truncate(sizeof(shared_memory_buffer));
+//    shm.truncate(sizeof(shared_memory_buffer));
+
+//    //Map the whole shared memory in this process
+//    mapped_region region
+//       (shm                       //What to map
+//       ,read_write //Map it as read-write
+//       );
+
+//    //Get the address of the mapped region
+//    void * addr       = region.get_address();
+
+//    //Construct the shared structure in memory
+//    shared_memory_buffer * data = new (addr) shared_memory_buffer;
+
+
+//    const int NumMsg = 10;
+
+//    //Insert data in the array
+//    for(int i = 0; i < NumMsg; ++i){
+//       data->nempty.wait();
+//       data->mutex.wait();
+//       data->items[i % shared_memory_buffer::NumItems] = i;
+//       printf("wrote: %d\n",i );
+//       data->mutex.post();
+//       data->nstored.post();
+//    }
+
+//    //Create a shared memory object.
+//    shared_memory_object vic_shm
+//       (open_only                    //only create
+//       ,"vic_MySharedMemory"              //name
+//       ,read_write  //read-write mode
+//       );
 
 //    //Map the whole shared memory in this process
 //    mapped_region vic_region
@@ -156,63 +193,32 @@ int main ()
 //       ,read_write //Map it as read-write
 //       );
 
-//    //Get the address of the mapped vic_region
+//    //Get the address of the mapped region
 //    void * vic_addr       = vic_region.get_address();
 
-//    //Construct the shared structure in memory
-//    shared_memory_buffer * vic_data = new (vic_addr) shared_memory_buffer;
-
-
-
-
-
-//    //Create a shared memory object.
-//    shared_memory_object shm
-//       (open_only                    //only create
-//       ,"MySharedMemory"              //name
-//       ,read_write  //read-write mode
-//       );
-
-//    //Map the whole shared memory in this process
-//    mapped_region region
-//       (shm                       //What to map
-//       ,read_write //Map it as read-write
-//       );
-
-//    //Get the address of the mapped region
-//    void * addr       = region.get_address();
-
 //    //Obtain the shared structure
-//    shared_memory_buffer * data = static_cast<shared_memory_buffer*>(addr);
+//    shared_memory_buffer * vic_data = static_cast<shared_memory_buffer*>(vic_addr);
 
-//    const int NumMsg = 10;
 
-//    int extracted_data [NumMsg];
+//    int extracted_vic_data [NumMsg];
 
 //    //Extract the data
 //    for(int i = 0; i < NumMsg; ++i){
-//       data->nstored.wait();
-//       data->mutex.wait();
-//       extracted_data[i] = data->items[i % shared_memory_buffer::NumItems];
-//       printf("got: %d\n",extracted_data[i]);
-//       data->mutex.post();
-//       data->nempty.post();
-//    }
-
-//    //Insert data in the array
-//    for(int i = 0; i < NumMsg; ++i){
-//       vic_data->nempty.wait();
+//       vic_data->nstored.wait();
 //       vic_data->mutex.wait();
-//       vic_data->items[i % shared_memory_buffer::NumItems] = i+10;
-//       printf("wrote: %d\n",i );
+//       extracted_vic_data[i] = vic_data->items[i % shared_memory_buffer::NumItems];
+//       printf("got: %d\n",extracted_vic_data[i]);
 //       vic_data->mutex.post();
-//       vic_data->nstored.post();
+//       vic_data->nempty.post();
 //    }
-
 
 //    return 0;
 // }
-// //The second process opens the shared memory and copies the received integers to it's own buffer:
+
+
+
+// //This is the process main process. Creates the shared memory, 
+// //places there the integer array and starts integers one by one, blocking if the array is full:
 
 // #include <boost/interprocess/sync/interprocess_semaphore.hpp>
 
@@ -231,7 +237,6 @@ int main ()
 //    //Items to fill
 //    int items[NumItems];
 // };
-
 // #include <boost/interprocess/shared_memory_object.hpp>
 // #include <boost/interprocess/mapped_region.hpp>
 // #include <iostream>
@@ -241,18 +246,22 @@ int main ()
 
 // int main ()
 // {
-//    //Remove shared memory on destruction
+//    //Remove shared memory on construction and destruction
 //    struct shm_remove
 //    {
+//       shm_remove() { shared_memory_object::remove("MySharedMemory"); }
 //       ~shm_remove(){ shared_memory_object::remove("MySharedMemory"); }
 //    } remover;
 
 //    //Create a shared memory object.
 //    shared_memory_object shm
-//       (open_only                    //only create
+//       (create_only                  //only create
 //       ,"MySharedMemory"              //name
 //       ,read_write  //read-write mode
 //       );
+
+//    //Set size
+//    shm.truncate(sizeof(shared_memory_buffer));
 
 //    //Map the whole shared memory in this process
 //    mapped_region region
@@ -263,21 +272,20 @@ int main ()
 //    //Get the address of the mapped region
 //    void * addr       = region.get_address();
 
-//    //Obtain the shared structure
-//    shared_memory_buffer * data = static_cast<shared_memory_buffer*>(addr);
+//    //Construct the shared structure in memory
+//    shared_memory_buffer * data = new (addr) shared_memory_buffer;
 
 //    const int NumMsg = 10;
 
-//    int extracted_data [NumMsg];
-
-//    //Extract the data
+//    //Insert data in the array
 //    for(int i = 0; i < NumMsg; ++i){
-//       data->nstored.wait();
+//       data->nempty.wait();
 //       data->mutex.wait();
-//       extracted_data[i] = data->items[i % shared_memory_buffer::NumItems];
-//       printf("got: %d\n",extracted_data[i]);
+//       data->items[i % shared_memory_buffer::NumItems] = i;
+//       printf("wrote: %d\n",i );
 //       data->mutex.post();
-//       data->nempty.post();
+//       data->nstored.post();
 //    }
+
 //    return 0;
 // }
--- ipc/shm/srv.cpp
+++ ipc/shm/srv.cpp
@@ -1,15 +1,14 @@
 // w create 2 mem, w read one, r write anthoer
-//This is the process main process. Creates the shared memory, 
-//places there the integer array and starts integers one by one, blocking if the array is full:
+//The second process opens the shared memory and copies the received integers to it's own buffer:
 
 #include <boost/interprocess/sync/interprocess_semaphore.hpp>
 
 struct shared_memory_buffer
 {
-   enum { NumItems = 5 };
+   enum { NumItems = 1 };
 
    shared_memory_buffer()
-      : mutex(1), nempty(NumItems), nstored(0)
+      : mutex(1), nempty(1), nstored(0)
    {}
 
    //Semaphores to protect and synchronize access
@@ -19,6 +18,7 @@ struct shared_memory_buffer
    //Items to fill
    int items[NumItems];
 };
+
 #include <boost/interprocess/shared_memory_object.hpp>
 #include <boost/interprocess/mapped_region.hpp>
 #include <iostream>
@@ -28,12 +28,15 @@ using namespace boost::interprocess;
 
 int main ()
 {
-   //Remove shared memory on construction and destruction
+   //Remove shared memory on destruction
    struct shm_remove
    {
-      shm_remove() { shared_memory_object::remove("MySharedMemory"); shared_memory_object::remove("vic_MySharedMemory");}
-      ~shm_remove(){ shared_memory_object::remove("MySharedMemory"); shared_memory_object::remove("vic_MySharedMemory");}
+      ~shm_remove(){ shared_memory_object::remove("MySharedMemory");shared_memory_object::remove("docker_MySharedMemory"); }
    } remover;
+
+
+
+
 
    //Create a shared memory object.
    shared_memory_object shm
@@ -41,9 +44,6 @@ int main ()
       ,"MySharedMemory"              //name
       ,read_write  //read-write mode
       );
-
-   //Set size
-   shm.truncate(sizeof(shared_memory_buffer));
 
    //Map the whole shared memory in this process
    mapped_region region
@@ -54,63 +54,54 @@ int main ()
    //Get the address of the mapped region
    void * addr       = region.get_address();
 
-   //Construct the shared structure in memory
-   shared_memory_buffer * data = new (addr) shared_memory_buffer;
+   //Obtain the shared structure
+   shared_memory_buffer * data = static_cast<shared_memory_buffer*>(addr);
+
+   const int NumMsg = 1;
+
+   int extracted_data [NumMsg];
+   int i = 0
+   //Extract the data
+   data->nstored.wait();
+   data->mutex.wait();
+   extracted_data[0] = data->items[0];
+   printf("srv got: %d\n",extracted_data[0]);
+   data->mutex.post();
+   data->nempty.post();
 
 
    //Create a shared memory object.
-   shared_memory_object vic_shm
-      (create_only                  //only create
-      ,"vic_MySharedMemory"              //name
+   shared_memory_object docker_shm
+      (open_only                  //only create
+      ,"docker_MySharedMemory"              //name
       ,read_write  //read-write mode
       );
-
-   //Set size
-   vic_shm.truncate(sizeof(shared_memory_buffer));
-
    //Map the whole shared memory in this process
-   mapped_region vic_region
-      (vic_shm                       //What to map
+   mapped_region docker_region
+      (docker_shm                       //What to map
       ,read_write //Map it as read-write
       );
 
+   //Get the address of the mapped docker_region
+   void * docker_addr       = docker_region.get_address();
 
-   const int NumMsg = 10;
-
-   //Insert data in the array
-   for(int i = 0; i < NumMsg; ++i){
-      data->nempty.wait();
-      data->mutex.wait();
-      data->items[i % shared_memory_buffer::NumItems] = i;
-      printf("wrote: %d\n",i );
-      data->mutex.post();
-      data->nstored.post();
-   }
+   //Construct the shared structure in memory
+   shared_memory_buffer * docker_data = new (docker_addr) shared_memory_buffer;
+   docker_data->nempty.wait();
+   docker_data->mutex.wait();
+   docker_data->items[0] = extracted_data[0];
+   printf("srv wrote: %d\n",docker_data->items[0] );
+   docker_data->mutex.post();
+   docker_data->nstored.post();
 
 
-   void * vic_addr       = vic_region.get_address();
-
-   //Obtain the shared structure
-   shared_memory_buffer * vic_data = static_cast<shared_memory_buffer*>(vic_addr);
-   int extracted_vic_data [NumMsg];
-
-   //Extract the data
-   for(int i = 0; i < NumMsg; ++i){
-      vic_data->nstored.wait();
-      vic_data->mutex.wait();
-      extracted_vic_data[i] = vic_data->items[i % shared_memory_buffer::NumItems];
-      printf("got: %d\n",extracted_vic_data[i]);
-      vic_data->mutex.post();
-      vic_data->nempty.post();
-   }
 
    return 0;
 }
 
+// w create sharedMem, open docker_shared. r read sharedMem, create docker_shared
 
-// w create sharedMem, open vic_shared. r read sharedMem, create vic_shared
-// //This is the process main process. Creates the shared memory, 
-// //places there the integer array and starts integers one by one, blocking if the array is full:
+// //The second process opens the shared memory and copies the received integers to it's own buffer:
 
 // #include <boost/interprocess/sync/interprocess_semaphore.hpp>
 
@@ -129,6 +120,7 @@ int main ()
 //    //Items to fill
 //    int items[NumItems];
 // };
+
 // #include <boost/interprocess/shared_memory_object.hpp>
 // #include <boost/interprocess/mapped_region.hpp>
 // #include <iostream>
@@ -138,22 +130,44 @@ int main ()
 
 // int main ()
 // {
-//    //Remove shared memory on construction and destruction
+//    //Remove shared memory on destruction
 //    struct shm_remove
 //    {
-//       shm_remove() { shared_memory_object::remove("MySharedMemory"); shared_memory_object::remove("vic_MySharedMemory");}
-//       ~shm_remove(){ shared_memory_object::remove("MySharedMemory"); shared_memory_object::remove("vic_MySharedMemory");}
+//       ~shm_remove(){ shared_memory_object::remove("MySharedMemory");shared_memory_object::remove("docker_MySharedMemory"); }
 //    } remover;
 
 //    //Create a shared memory object.
-//    shared_memory_object shm
+//    shared_memory_object docker_shm
 //       (create_only                  //only create
-//       ,"MySharedMemory"              //name
+//       ,"docker_MySharedMemory"              //name
 //       ,read_write  //read-write mode
 //       );
 
 //    //Set size
-//    shm.truncate(sizeof(shared_memory_buffer));
+//    docker_shm.truncate(sizeof(shared_memory_buffer));
+
+//    //Map the whole shared memory in this process
+//    mapped_region docker_region
+//       (docker_shm                       //What to map
+//       ,read_write //Map it as read-write
+//       );
+
+//    //Get the address of the mapped docker_region
+//    void * docker_addr       = docker_region.get_address();
+
+//    //Construct the shared structure in memory
+//    shared_memory_buffer * docker_data = new (docker_addr) shared_memory_buffer;
+
+
+
+
+
+//    //Create a shared memory object.
+//    shared_memory_object shm
+//       (open_only                    //only create
+//       ,"MySharedMemory"              //name
+//       ,read_write  //read-write mode
+//       );
 
 //    //Map the whole shared memory in this process
 //    mapped_region region
@@ -164,61 +178,37 @@ int main ()
 //    //Get the address of the mapped region
 //    void * addr       = region.get_address();
 
-//    //Construct the shared structure in memory
-//    shared_memory_buffer * data = new (addr) shared_memory_buffer;
-
+//    //Obtain the shared structure
+//    shared_memory_buffer * data = static_cast<shared_memory_buffer*>(addr);
 
 //    const int NumMsg = 10;
 
-//    //Insert data in the array
-//    for(int i = 0; i < NumMsg; ++i){
-//       data->nempty.wait();
-//       data->mutex.wait();
-//       data->items[i % shared_memory_buffer::NumItems] = i;
-//       printf("wrote: %d\n",i );
-//       data->mutex.post();
-//       data->nstored.post();
-//    }
-
-//    //Create a shared memory object.
-//    shared_memory_object vic_shm
-//       (open_only                    //only create
-//       ,"vic_MySharedMemory"              //name
-//       ,read_write  //read-write mode
-//       );
-
-//    //Map the whole shared memory in this process
-//    mapped_region vic_region
-//       (vic_shm                       //What to map
-//       ,read_write //Map it as read-write
-//       );
-
-//    //Get the address of the mapped region
-//    void * vic_addr       = vic_region.get_address();
-
-//    //Obtain the shared structure
-//    shared_memory_buffer * vic_data = static_cast<shared_memory_buffer*>(vic_addr);
-
-
-//    int extracted_vic_data [NumMsg];
+//    int extracted_data [NumMsg];
 
 //    //Extract the data
 //    for(int i = 0; i < NumMsg; ++i){
-//       vic_data->nstored.wait();
-//       vic_data->mutex.wait();
-//       extracted_vic_data[i] = vic_data->items[i % shared_memory_buffer::NumItems];
-//       printf("got: %d\n",extracted_vic_data[i]);
-//       vic_data->mutex.post();
-//       vic_data->nempty.post();
+//       data->nstored.wait();
+//       data->mutex.wait();
+//       extracted_data[i] = data->items[i % shared_memory_buffer::NumItems];
+//       printf("got: %d\n",extracted_data[i]);
+//       data->mutex.post();
+//       data->nempty.post();
 //    }
+
+//    //Insert data in the array
+//    for(int i = 0; i < NumMsg; ++i){
+//       docker_data->nempty.wait();
+//       docker_data->mutex.wait();
+//       docker_data->items[i % shared_memory_buffer::NumItems] = i+10;
+//       printf("wrote: %d\n",i );
+//       docker_data->mutex.post();
+//       docker_data->nstored.post();
+//    }
+
 
 //    return 0;
 // }
-
-
-
-// //This is the process main process. Creates the shared memory, 
-// //places there the integer array and starts integers one by one, blocking if the array is full:
+// //The second process opens the shared memory and copies the received integers to it's own buffer:
 
 // #include <boost/interprocess/sync/interprocess_semaphore.hpp>
 
@@ -237,6 +227,7 @@ int main ()
 //    //Items to fill
 //    int items[NumItems];
 // };
+
 // #include <boost/interprocess/shared_memory_object.hpp>
 // #include <boost/interprocess/mapped_region.hpp>
 // #include <iostream>
@@ -246,22 +237,18 @@ int main ()
 
 // int main ()
 // {
-//    //Remove shared memory on construction and destruction
+//    //Remove shared memory on destruction
 //    struct shm_remove
 //    {
-//       shm_remove() { shared_memory_object::remove("MySharedMemory"); }
 //       ~shm_remove(){ shared_memory_object::remove("MySharedMemory"); }
 //    } remover;
 
 //    //Create a shared memory object.
 //    shared_memory_object shm
-//       (create_only                  //only create
+//       (open_only                    //only create
 //       ,"MySharedMemory"              //name
 //       ,read_write  //read-write mode
 //       );
-
-//    //Set size
-//    shm.truncate(sizeof(shared_memory_buffer));
 
 //    //Map the whole shared memory in this process
 //    mapped_region region
@@ -272,20 +259,21 @@ int main ()
 //    //Get the address of the mapped region
 //    void * addr       = region.get_address();
 
-//    //Construct the shared structure in memory
-//    shared_memory_buffer * data = new (addr) shared_memory_buffer;
+//    //Obtain the shared structure
+//    shared_memory_buffer * data = static_cast<shared_memory_buffer*>(addr);
 
 //    const int NumMsg = 10;
 
-//    //Insert data in the array
-//    for(int i = 0; i < NumMsg; ++i){
-//       data->nempty.wait();
-//       data->mutex.wait();
-//       data->items[i % shared_memory_buffer::NumItems] = i;
-//       printf("wrote: %d\n",i );
-//       data->mutex.post();
-//       data->nstored.post();
-//    }
+//    int extracted_data [NumMsg];
 
+//    //Extract the data
+//    for(int i = 0; i < NumMsg; ++i){
+//       data->nstored.wait();
+//       data->mutex.wait();
+//       extracted_data[i] = data->items[i % shared_memory_buffer::NumItems];
+//       printf("got: %d\n",extracted_data[i]);
+//       data->mutex.post();
+//       data->nempty.post();
+//    }
 //    return 0;
 // }
