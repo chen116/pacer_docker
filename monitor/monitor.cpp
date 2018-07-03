0c181fa3b873de9a1b28e8a7f047491f580d5444
Victor Chen <yvictorck@gmail.com>
2018-07-03 15:32:30 -0400

    a

--- monitor/monitor.cpp
+++ monitor/monitor.cpp
@@ -41,6 +41,37 @@ int main (int argc, char **argv)
     char in_buffer [MSG_BUFFER_SIZE];
     char out_buffer [MSG_BUFFER_SIZE];
 
+
+
+    // setup
+    // get the oldest message with highest priority
+    if (mq_receive (qd_server, in_buffer, MSG_BUFFER_SIZE, NULL) == -1) {
+        perror ("Server: mq_receive");
+        exit (1);
+    }
+    printf ("Server: message received:%s\n",in_buffer);
+    if ((qd_client = mq_open (in_buffer, O_WRONLY)) == 1) {
+        perror ("Server: Not able to open client queue");
+        continue;
+    }
+
+
+
+    int i;char bs[1];
+	sscanf(in_buffer, "%s%d", bs,&i);
+	int shmkey = atoi(in_buffer);
+	printf("shmkey %d\n",shmkey);
+	double tempRetVal;
+		int shmid;
+	if ((shmid = shmget(shmkey, 1*sizeof(heartbeat_t), 0666)) < 0) {
+	    perror("shmget");
+	    return 0;
+	}
+	heartbeat_t* hb = (heartbeat_t*) shmat(shmid, NULL, 0);
+
+
+
+	int cnt = 0
     while (1) {
         // mq_receive (qd_server, in_buffer, MSG_BUFFER_SIZE, NULL);
         // qd_client = mq_open (in_buffer, O_WRONLY);
@@ -49,28 +80,33 @@ int main (int argc, char **argv)
         
 
     
+
+
         // get the oldest message with highest priority
         if (mq_receive (qd_server, in_buffer, MSG_BUFFER_SIZE, NULL) == -1) {
             perror ("Server: mq_receive");
             exit (1);
         }
-        printf ("Server: message received:%s\n",in_buffer);
-        // send reply message to client
-        if ((qd_client = mq_open (in_buffer, O_WRONLY)) == 1) {
-            perror ("Server: Not able to open client queue");
-            continue;
-        }
-        int i;
-        char bs[1];
-		sscanf(in_buffer, "%s%d", bs,&i);
-		int shmkey = atoi(in_buffer);
-		printf("shmkey %d %d\n",shmkey,i);
+	    printf ("Server: message received:%s\n",in_buffer);
+	    if ((qd_client = mq_open (in_buffer, O_WRONLY)) == 1) {
+	        perror ("Server: Not able to open client queue");
+	        continue;
+	    }
 
+		tempRetVal = hb->log[0].instant_rate;
+		printf("hb: %f\n",tempRetVal );
+		cnt++;
         sprintf (out_buffer, "%d", token_number);
+
         if (mq_send (qd_client, out_buffer, strlen (out_buffer) + 1, 0) == -1) {
             perror ("Server: Not able to send message to client");
             continue;
         }
+
+
+
+
+
         mq_close(qd_client);
         printf ("Server: response sent to client. %d\n",token_number);
         token_number++;
