707f55a0a6184df5e2d3e275df7839a3ce35694c
Victor Chen <yvictorck@gmail.com>
2018-07-04 17:48:49 -0400

    a

--- monitor/monitor.cpp
+++ monitor/monitor.cpp
@@ -114,7 +114,7 @@ int main (int argc, char **argv)
 		tempRetVal = hb_rec->instant_rate;
 		printf("hb rec: %f\n",tempRetVal );
 
-		heartbeat_record_t* meow = hb->log;
+		heartbeat_record_t* meow = hb->logf;
 		printf("hb: %f\n",meow->instant_rate );
 		cnt++;
 
