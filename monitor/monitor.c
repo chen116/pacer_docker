78d019880aa62ff0f5a4d9c9f7ec11c53e6aa154
Victor Chen <yvictorck@gmail.com>
2018-07-05 16:03:31 -0400

    a

--- monitor/monitor.c
+++ monitor/monitor.c
@@ -47,19 +47,6 @@ int main (int argc, char **argv)
 
 
 
-    // setup
-    // get the oldest message with highest priority
-    if (mq_receive (qd_server, in_buffer, MSG_BUFFER_SIZE, NULL) == -1) {
-        perror ("Server: mq_receive");
-        exit (1);
-    }
-    printf ("Server: message received in init:%s\n",in_buffer);
-    if ((qd_client = mq_open (in_buffer, O_WRONLY)) == 1) {
-        perror ("Server: Not able to open client queue");
-
-    }
-
-
 
     int i;char bs[1];
 	sscanf(in_buffer, "%s%d", bs,&i);
@@ -74,20 +61,20 @@ int main (int argc, char **argv)
 	// }
 	// heartbeat_t* hb = (heartbeat_t*) shmat(shmid, NULL, 0);
 
-	int shmid_rec;
-	if ((shmid_rec = shmget(shmkey << 1, 100*sizeof(heartbeat_record_t), 0666)) < 0) {
-	    perror("shmget_rec");
-	    return 0;
-	}
-	heartbeat_record_t* hb_rec = (heartbeat_record_t*) shmat(shmid_rec, NULL, 0);
+    // int shmid_rec;
+    // if ((shmid_rec = shmget(shmkey << 1, 100*sizeof(heartbeat_record_t), 0666)) < 0) {
+    //     perror("shmget_rec");
+    //     return 0;
+    // }
+    // heartbeat_record_t* hb_rec = (heartbeat_record_t*) shmat(shmid_rec, NULL, 0);
 
 
-    int shmid_state;
-    if ((shmid_state = shmget( (shmkey << 1) | 1, sizeof(HB_global_state_t), 0666)) < 0) {
-        perror("shmget2");
-        return 0;
-    }
-    HB_global_state_t* hb_state = (HB_global_state_t*) shmat(shmid_state, NULL, 0);
+    // int shmid_state;
+    // if ((shmid_state = shmget( (shmkey << 1) | 1, sizeof(HB_global_state_t), 0666)) < 0) {
+    //     perror("shmget2");
+    //     return 0;
+    // }
+    // HB_global_state_t* hb_state = (HB_global_state_t*) shmat(shmid_state, NULL, 0);
 
 
 
@@ -96,7 +83,7 @@ int main (int argc, char **argv)
 
 
     heartbeat_record_t* init_hb_rec = hb_rec;
-
+    int seen = 0 ;
 
     while (1) {
         // mq_receive (qd_server, in_buffer, MSG_BUFFER_SIZE, NULL);
@@ -118,6 +105,30 @@ int main (int argc, char **argv)
 	        perror ("Server: Not able to open client queue");
 	        continue;
 	    }
+
+        if (seen==0)
+        {
+            seen=1;
+    int shmid_rec;
+    if ((shmid_rec = shmget(shmkey << 1, 100*sizeof(heartbeat_record_t), 0666)) < 0) {
+        perror("shmget_rec");
+        return 0;
+    }
+    heartbeat_record_t* hb_rec = (heartbeat_record_t*) shmat(shmid_rec, NULL, 0);
+
+
+    int shmid_state;
+    if ((shmid_state = shmget( (shmkey << 1) | 1, sizeof(HB_global_state_t), 0666)) < 0) {
+        perror("shmget2");
+        return 0;
+    }
+    HB_global_state_t* hb_state = (HB_global_state_t*) shmat(shmid_state, NULL, 0);
+
+
+        }
+
+
+
 
         hb_rec=    init_hb_rec +      hb_state->counter-1;
         printf("hb rec:%s %f\n",in_buffer,hb_rec->instant_rate );
