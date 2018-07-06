470b38f3e5ce4174932c9ad6b22d706a29c980cf
Victor Chen <yvictorck@gmail.com>
2018-07-06 17:35:53 -0400

    a

--- monitor/monitor.cpp
+++ monitor/monitor.cpp
@@ -178,12 +178,15 @@ public:
                     printf("closing client with pid %d\n",pid);
                     mq_close(map[pid].qd_client);
 
-                    if (shmdt(map[pid].init_hb_rec)==0 && shmctl(pid << 1, IPC_RMID, NULL)==0)
+                    if (shmdt(map[pid].init_hb_rec)<0)// && shmctl(pid << 1, IPC_RMID, NULL)==0)
                     {
+                        perror("hbrec shmdt");
                         printf("shmdt hb_rec success\n");
                     }
-                    if (shmdt(map[pid].hb_state)==0 && shmctl((pid << 1) | 1, IPC_RMID, NULL)==0)
+                    if (shmdt(map[pid].hb_state)<0 )//&& shmctl((pid << 1) | 1, IPC_RMID, NULL)==0)
                     {
+                        perror("hbstate shmdt");
+
                         printf("shmdt hb_state success\n");
                     }
                     map.erase(pid);
